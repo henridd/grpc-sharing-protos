51d5f0e4a92f5d25ad6367fb973bdeeaa5c87678
henridd <henri.oo@hotmail.com>
2024-02-26 09:48:56 +0100

    Initial commit

---
+++ Client/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using Protos;
+
+var grpcChannel = GrpcChannel.ForAddress("https://localhost:7161");
+var greeter = new Greeter.GreeterClient(grpcChannel);
+var request = new HelloRequest() { Name = "World" };
+var response = await greeter.SayHelloAsync(request);
+Console.WriteLine(response);
+Console.ReadLine();
